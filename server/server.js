f0f51ddf87369c56b0b335fb76365bf83cb979f9
shalom <shalomshobowale65@gmail.com>
2025-05-17 22:17:21 +0100

    commit-1

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = ["http://localhost:5173","https://fitness-3wke.onrender.com"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "https://fitness-3wke.onrender.com"];
 
 // ✅ UPDATED CORS CONFIG
 app.use(
